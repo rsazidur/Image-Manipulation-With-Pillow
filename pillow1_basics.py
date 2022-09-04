f99df4215989018b62e83634c1d9e8873e7ff27c
Molla Sazidur Rahman <rsazidur936@gmail.com>
2022-09-05 02:04:34 +0600

    Basic Image Operations

--- pillow1_basics.py
+++ pillow1_basics.py
@@ -1,7 +1,23 @@
 from PIL import Image
 
-# create new image by import
+# create new image by import...
 image = Image.open('Cat.jpg')
 
-# show the picture
-image.show()
+# alternative way to import an image...
+# with Image.open('Cat.jpg') as image:
+#     image.show()
+
+# create a new image from scratch...
+image_blank = Image.new('RGBA', (1000, 600))
+
+# show the picture...
+# image_blank.show()
+
+# saving the picture...
+# image.save('test_save.png')
+
+# image information...
+print(image.size)
+print(image.filename)
+print(image.format)
+print(image.format_description)
